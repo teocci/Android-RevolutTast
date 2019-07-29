26a1c052e42bb1e9744c875cce38af67786ed794
MSI Teocci <teocci@yandex.com>
2019-07-30 02:56:43 +0900

    first commit

---
+++ app/src/main/java/com/github/teocci/taskrevolut/utils/Config.java
@@ -0,0 +1,15 @@
+package com.github.teocci.taskrevolut.utils;
+
+/**
+ * Created by Teocci.
+ *
+ * @author teocci@yandex.com on 2019-Jul-30
+ */
+public class Config
+{
+    public final static String APP_NAME = "TR";
+    public final static String LOG_PREFIX = "[" + APP_NAME + "]";
+
+    public final static String BASE_CURRENCY_TYPE= "EUR";
+    public final static String BASE_URL= "https://revolut.duckdns.org/latest?";
+}
